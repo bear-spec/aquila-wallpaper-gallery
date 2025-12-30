8327cea5e96ba74b11ff04f41e9b5a5103e1518d
bear-spec <iamrathin578@gmail.com>
2025-12-30 22:18:41 +0530

    Update contactj.js

--- js/contactj.js
+++ js/contactj.js
@@ -1,30 +1,18 @@
-    function menushow() {
-        var sidenav = document.querySelector(".side-nav")
-        sidenav.style.left = "0"
+function menushow() {
+    document.querySelector(".side-nav").style.left = "0"
+}
 
-    }
+function menuhide() {
+    document.querySelector(".side-nav").style.left = "-70%"
+}
 
-    function menuhide() {
-        var sidenav = document.querySelector(".side-nav")
-        sidenav.style.left = "-50%"
-    }
-    function searchelmt(event){
-    var prodcont=document.getElementById("product")
-    var search=document.getElementById("search")
-    var prodlist=prodcont.querySelectorAll("div")
+function searchelmt(event) {
+    var prodcont = document.getElementById("product")
+    var enteredval = event.target.value.toUpperCase()
+    var prodlist = prodcont.querySelectorAll(".product-box")
 
-    var enteredval=event.target.value.toUpperCase()
-
-    for(var count=0;count<prodlist.length;count=count+1)
-    {
-        var prodname=prodlist[count].querySelector("p").textContent
-
-        if(prodname.toUpperCase().indexOf(enteredval)<0)
-    {
-        prodlist[count].style.display="none"
-    }
-    else{
-        prodlist[count].style.display="block"
-    }
-    }
-    }
+    prodlist.forEach(function (item) {
+        var prodname = item.querySelector("p").textContent.toUpperCase()
+        item.style.display = prodname.includes(enteredval) ? "block" : "none"
+    })
+}
